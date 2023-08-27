bf42fd1e85808bd80f3c02d2f535cf6491e10d78
Tamirlan <tuarshintamir@gmail.com>
2023-08-27 15:51:00 +0500

    project details, add swiper in project

---
+++ vue7/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <HeaderComponent />
+  <PropsStyle />
+  <div class="banner"><img class="banner__img" src="./assets/banner.jpg" alt="mainBanner"></div>
+
+  <div class="main">
+    <div class="content center">
+      <h2 class="header__content">
+        {{ headerContent }}
+      </h2>
+      <p class="header__text">
+        {{ paragraphOne }}
+        <br>
+        <br>
+        {{ paragraphTwo }}
+      </p>
+    </div>
+  </div>
+  <SwiperProps />
+
+  <FooterComponent />
+</template>
+
+<script>
+import HeaderComponent from './components/Header.vue';
+import FooterComponent from './components/Footer.vue';
+import PropsStyle from './components/style.vue';
+import SwiperProps from './components/Swiper.vue';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'App',
+
+  components: {
+    HeaderComponent,
+    FooterComponent,
+    PropsStyle,
+    SwiperProps
+  },
+  computed: {
+    ...mapState(['headerContent', 'paragraphOne', 'paragraphTwo'])
+  }
+}
+</script>
+
+<style>
+.banner {
+  padding-bottom: 40px;
+  width: 100%;
+  flex-shrink: 0;
+}
+
+.banner__img {
+  width: 100%;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+}
+
+.content {
+  color: #4D5053;
+  font-family: Jost;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+  /* 33px */
+  letter-spacing: 0.22px;
+  width: 638px;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 11px;
+  padding-bottom: 100px;
+}
+
+.header__content {
+  color: #292F36;
+  font-family: DM Serif Display;
+  font-size: 50px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 125%;
+  /* 62.5px */
+  letter-spacing: 1px;
+}
+
+.header__text {
+  color: #4D5053;
+  font-family: Jost;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+  /* 33px */
+  letter-spacing: 0.22px;
+}
+</style>
---
+++ vue7/src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div class="footer center">
+            <div class="footer__inferno">
+                <div class="footer__logo">
+                    <a href="#" class="logo__link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+                            <path
+                                d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
+                                fill="#CDA274" />
+                        </svg>
+                    </a>
+                    <p class="footer__img">Interno</p>
+                </div>
+                <p class="footer__text">It is a long established fact that a reader will be distracted lookings.</p>
+                <div class="footer__icon">
+                    <img src="../assets/twitter__logo.svg" alt="twitter_logo">
+                    <img src="../assets/In.svg" alt="in__logo">
+                </div>
+            </div>
+            <div class="footer__pages">
+                <h2 class="pages__title">Pages</h2>
+                <div class="pages__links">
+                    <a class="pages__link" href="#">Home</a>
+                    <a class="pages__link" href="#">Project</a>
+                    <a class="pages__link" href="#">Blog</a>
+                </div>
+            </div>
+            <div class="services"></div>
+            <div class="footer__contacts">
+                <p class="contacts__title">Contacts</p>
+                <div class="contacts__text">
+                    <p class="contacts__text">55 East Birchwood Ave. Brooklyn, New York 11201</p>
+                    <p class="contacts__text">contact@interno.com</p>
+                    <p class="contacts__text">(123) 456 - 7890</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped></style>
